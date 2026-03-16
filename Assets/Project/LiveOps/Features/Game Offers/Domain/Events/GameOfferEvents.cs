a449d40c1d1d80194d56766a31b3e1920def99dd
WINDOWS-PC\datnt <thedat071298@gmail.com>
2026-03-16 09:30:04 +0700

    fix

--- Assets/Project/LiveOps/Features/Game Offers/Domain/Events/GameOfferEvents.cs
+++ Assets/Project/LiveOps/Features/Game Offers/Domain/Events/GameOfferEvents.cs
@@ -1,62 +1,38 @@
-using System;
+using R3;
+using GameOfferSystem.Domain;
 
-/// <summary>
-/// Dispatcher các sự kiện của hệ thống Game Offer.
-/// Các hệ khác như UI, Analytics, Gameplay có thể subscribe các event này.
-/// </summary>
 public class GameOfferEvents : IGameOfferEvents
 {
-    /// <summary>
-    /// Sự kiện khi một Offer được kích hoạt.
-    /// </summary>
-    public event Action<GameOfferRuntimeData> OfferActivated;
+    private readonly Subject<GameOfferRuntimeData> _offerActivated = new();
+    private readonly Subject<(GameOfferRuntimeData offer, bool wasPurchased)> _offerDeactivated = new();
+    private readonly Subject<GameOfferRuntimeData> _offerPurchased = new();
+    private readonly Subject<(string offerId, OfferPurchaseError error)> _offerPurchaseFailed = new();
 
-    /// <summary>
-    /// Sự kiện khi Offer bị tắt hoặc hết hạn.
-    /// bool wasPurchased = true nếu player đã mua offer trước khi tắt.
-    /// </summary>
-    public event Action<GameOfferRuntimeData, bool> OfferDeactivated;
+    public Observable<GameOfferRuntimeData> OfferActivated => _offerActivated;
 
-    /// <summary>
-    /// Sự kiện khi player mua Offer thành công.
-    /// </summary>
-    public event Action<GameOfferRuntimeData> OfferPurchased;
+    public Observable<(GameOfferRuntimeData offer, bool wasPurchased)> OfferDeactivated => _offerDeactivated;
 
-    /// <summary>
-    /// Sự kiện khi player mua Offer thất bại.
-    /// error: lý do thất bại (ví dụ: không đủ tiền, offer hết hạn,...)
-    /// </summary>
-    public event Action<GameOfferRuntimeData, string> OfferPurchaseFailed;
+    public Observable<GameOfferRuntimeData> OfferPurchased => _offerPurchased;
 
-    /// <summary>
-    /// Trigger khi Offer được kích hoạt.
-    /// </summary>
+    public Observable<(string offerId, OfferPurchaseError error)> OfferPurchaseFailed => _offerPurchaseFailed;
+
     public void OnOfferActivated(GameOfferRuntimeData offer)
     {
-        OfferActivated?.Invoke(offer);
+        _offerActivated.OnNext(offer);
     }
 
-    /// <summary>
-    /// Trigger khi Offer bị tắt hoặc hết hạn.
-    /// </summary>
     public void OnOfferDeactivated(GameOfferRuntimeData offer, bool wasPurchased)
     {
-        OfferDeactivated?.Invoke(offer, wasPurchased);
+        _offerDeactivated.OnNext((offer, wasPurchased));
     }
 
-    /// <summary>
-    /// Trigger khi player mua Offer thành công.
-    /// </summary>
     public void OnOfferPurchased(GameOfferRuntimeData offer)
     {
-        OfferPurchased?.Invoke(offer);
+        _offerPurchased.OnNext(offer);
     }
 
-    /// <summary>
-    /// Trigger khi player mua Offer thất bại.
-    /// </summary>
-    public void OnOfferPurchaseFailed(GameOfferRuntimeData offer, string error)
+    public void OnOfferPurchaseFailed(string offerId, OfferPurchaseError error)
     {
-        OfferPurchaseFailed?.Invoke(offer, error);
+        _offerPurchaseFailed.OnNext((offerId, error));
     }
 }
